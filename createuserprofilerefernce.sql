ca43c955d6d77376ae1a904ca5621c1604765bbb
galeNight <97892095+galeNight@users.noreply.github.com>
2023-11-10 12:49:28 +0100

    Add files via upload

---
+++ createuserprofilerefernce.sql
@@ -0,0 +1,35 @@
+USE [Dating]
+GO
+
+
+CREATE TABLE [dbo].[UserProfile](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[UserName] [nvarchar](50) NOT NULL,
+	[BirthDate] [datetime] NOT NULL,
+	[Height] [int] NOT NULL,
+	[AboutMe] [nvarchar](255) NULL,
+	[CityId] [int] NULL,
+	[GenderId] [int] NULL,
+	[UsersId] [int] NULL
+	)
+
+
+ALTER TABLE [dbo].[UserProfile] ADD  DEFAULT ((0)) FOR [Height]
+GO
+
+ALTER TABLE [dbo].[UserProfile]  WITH CHECK ADD FOREIGN KEY([CityId])
+REFERENCES [dbo].[City] ([Id])
+GO
+
+ALTER TABLE [dbo].[UserProfile]  WITH CHECK ADD FOREIGN KEY([GenderId])
+REFERENCES [dbo].[Gender] ([Id])
+GO
+
+ALTER TABLE [dbo].[UserProfile]  WITH CHECK ADD FOREIGN KEY([UsersId])
+REFERENCES [dbo].[Users] ([Id])
+GO
+
+ALTER TABLE [dbo].[UserProfile]  WITH CHECK ADD CHECK  (([BirthDate]<=dateadd(year,(-18),getdate())))
+GO
+
+
